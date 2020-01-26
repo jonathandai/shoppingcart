7c5aa299308b3a9b5a1dea9a59f31e94b2eb3001
Jonathan Dai <jonathandai@Jonathans-MacBook-Pro.local>
2020-01-25 21:47:51 -0600

    only instock inventory shown

--- src/App.js
+++ src/App.js
@@ -4,6 +4,22 @@ import { Card, Column, Container, Button } from "rbx";
 import { makeStyles } from "@material-ui/core/styles";
 import Divider from '@material-ui/core/Divider';
 import Sidebar from "react-sidebar";
+import "firebase/database";
+// import "firebase/auth";
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDpKL6lxJlwVKi2P4DyqkFKk_u6n5DqnbI",
+  authDomain: "jonathan-shopping-cart.firebaseapp.com",
+  databaseURL: "https://jonathan-shopping-cart.firebaseio.com",
+  projectId: "jonathan-shopping-cart",
+  storageBucket: "jonathan-shopping-cart.appspot.com",
+  messagingSenderId: "1054191744298",
+  appId: "1:1054191744298:web:dcd7fd4bd3347338391a53"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database().ref();
 
 const productStyles = makeStyles(theme => ({
   container: {
@@ -57,7 +73,7 @@ const productStyles = makeStyles(theme => ({
   cart: {
     marginLeft: 10,
     fontWeight: 'bold'
-  }
+  },
 }));
 
 
@@ -77,8 +93,22 @@ const App = () => {
   const [cart, setCart] = useState([])
   const [total, setTotal] = useState(0.0)
 
+  const [inventory, setInventory] = useState({});
+
+  useEffect(() => {
+    const handleData = snap => {
+      if (snap.val()) setInventory(snap.val());
+    }
+    db.on('value', handleData, error => alert(error));
+    return () => { db.off('value', handleData); };
+  }, []);
+
   const addItemToCart = (product, size) => { 
-    setTotal(total + product.price)
+    console.log(typeof total)
+    console.log(typeof product.price)
+    let newTotal = (total + product.price) 
+    // newTotal = typeof newTotal === 'string' ? newTotal.parseInt().toFixed : newTotal.toFixed(2)
+    setTotal(newTotal)
     for (var i = 0; i < cart.length; i++) {
       if(cart[i][0].sku === product.sku && cart[i][1] === size) {
         let new_value = cart[i][2] + 1
@@ -94,26 +124,60 @@ const App = () => {
   }
 
   const removeItemFromCart = (product, size) => { 
-    setTotal((total - product.price).toFixed(2))
-    for (var i = 0; i < cart.length; i++) {
-      if(cart[i][0].sku === product.sku && cart[i][1] === size) {
-        let new_value = cart[i][2] - 1
-        if(new_value === 0) {
-          cart.splice(i)
-        }
-        else {
-          cart[i] = new Array(product, size, new_value); 
-        }
-        setCart(cart)
-        setSidebar(true)
-        return; 
-      }
+    let newTotal = (total-product.price).toFixed(2)
+    setTotal(newTotal)
+    const itemIndex = cart.findIndex(item => item[0].sku === product.sku && item[1] === size) 
+    console.log(itemIndex)
+    let newCart 
+    if(cart[itemIndex][2] === 1) {
+      newCart = [...cart.slice(0, itemIndex), ...cart.slice(itemIndex+1)]
     }
+    else {
+      newCart = [...cart.slice(0, itemIndex), new Array(product, size, cart[itemIndex][2]-1),...cart.slice(itemIndex+1)]
+    }
+    setCart(newCart)
+    setSidebar(true)
+    // for (var i = 0; i < cart.length; i++) {
+    //   if(cart[i][0].sku === product.sku && cart[i][1] === size) {
+    //     let new_value = cart[i][2] - 1
+    //     if(new_value === 0) {
+    //       cart.splice(i)
+    //     }
+    //     else {
+    //       cart[i] = new Array(product, size, new_value); 
+    //     }
+    //     setCart(cart)
+    //     setSidebar(true)
+    //     return; 
+    //   }
+    // }
   }
   
+  const isInStock = (product, size) => {
+    
+    for(var i = 0; i < Object.keys(inventory).length; i++) {      
+      var currSku = Object.keys(inventory)[i]
+      if(parseInt(currSku) === product.sku) {
+        var sizesInStock = inventory[parseInt(currSku)]
+        return sizesInStock && sizesInStock[size] > 0 ? true : false 
+      }
+    }
+    return false 
+  }
+
+  const renderInStock = (product) => {
+    const sizes = ['S', 'M', 'L', 'XL']
+    return( 
+      <div>{sizes.map(size => 
+        isInStock(product, size) ?
+        <Button onClick={() => addItemToCart(product, size)} className={styles.sizes}>{size}</Button> :
+        <Button className={styles.sizes} disabled={true}>{size}</Button>
+        )}</div> 
+    )
+  }
+
   const ProductList = ({products}) => {
     const styles = productStyles();
-    const sizes = ['S', 'M', 'L', 'XL']
     return(
       <Column.Group vcentered multiline className={styles.container}>
         {products.map(product => 
@@ -122,7 +186,7 @@ const App = () => {
                     <Card.Image className={styles.image}><img src={`/data/products/${product.sku}_1.jpg`} alt={product.sku}/></Card.Image>
                     <Card.Content key={product.sku}> 
                       <h4 className={styles.title}>{product.title}</h4>
-                      {sizes.map(size => <Button onClick={() => addItemToCart(product, size)} className={styles.sizes}>{size}</Button>)}
+                      {renderInStock(product)}
                       <Divider variant="middle" className={styles.divider}/>
                       <h6 className={styles.price}>{`$${product.price}`}</h6>
                       <h6 className={styles.description}>{product.style}</h6>
@@ -149,6 +213,7 @@ const App = () => {
       <div>cart is empty</div>
     )
   }
+
 
   const styles = productStyles();
   return (
